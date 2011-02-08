2add4102e3b994a3a43f8360aa08b56ca28c66b4
Eric Teubert <ericteubert@googlemail.com>
2011-02-08 13:09:37 +0100

    initial population generation

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
+require 'tetris-api'
+
 class Individual
+  attr_reader :weights, :exponents
   
   @weights    = []
   @exponents  = []
@@ -9,11 +12,13 @@ class Individual
   end
 
   def self.create_random(size = 12)
-    weights = exponents = []
+    weights = []
+    exponents = []
     size.times do |i|
       weights << self::random_weight
       exponents << self::random_exponent
     end
+    Individual.new(:weights => weights, :exponents => exponents)
   end
   
   # random signed fixnum (-999 to 999)
@@ -30,3 +35,28 @@ class Individual
   
 end
 
+POPULATION_SIZE = 5
+
+class Main
+  
+  def initialize(values = {})
+    @population_size = values[:population_size] || POPULATION_SIZE
+    @population = []
+  end
+  
+  def run
+    generate_initial_population
+  end
+  
+  private
+  
+  def generate_initial_population
+    @population_size.times do |i|
+      @population << Individual::create_random
+    end
+  end
+  
+end
+
+main = Main.new
+main.run
